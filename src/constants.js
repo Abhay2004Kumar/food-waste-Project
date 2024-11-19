ad91cab3e5409acc709a813f3966b4741be00c53
Abhay2004Kumar <abs220502@gmail.com>
2024-11-19 15:34:16 +0530

    add utils

---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const DB_NAME = "food_waste_db"
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants.js";
+
+import {app} from "./app.js"
+
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './.env'
+})
+
+//professional approach
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MONGODB connection failed !",error);
+})
+
